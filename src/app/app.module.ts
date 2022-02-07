fc7b16df920a20b9751a7e4b065d73fb200a7649
pridon <fridontetradze@gmail.com>
2022-02-07 18:31:30 +0400

    chore: setup firebase and firestore.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Firebase firestore
+import { environment } from '../environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'primordial-soup-site',
+    appId: '1:287500756572:web:f065c642c5f71b34bf2899',
+    databaseURL: 'https://primordial-soup-site-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'primordial-soup-site.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCp1HQqMsNBcn-Hk7VsXirj183xxFAGyKU',
+    authDomain: 'primordial-soup-site.firebaseapp.com',
+    messagingSenderId: '287500756572',
+  },
 };
 
 /*
