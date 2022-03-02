e5d61be073c5003340485c1a80bf9a39febd423e
Fridon <fridontetradze@gmail.com>
2022-03-03 00:35:56 +0400

    fix(notification): not showing close action on articles list fail

--- src/app/reader/store/effects/reader.effects.ts
+++ src/app/reader/store/effects/reader.effects.ts
@@ -17,7 +17,8 @@ export class ReaderEffects {
           ),
           catchError((error) => {
             this.notificationService.notify(
-              `Failed to get articles list. Server responded with error ${error.error}`
+              `Failed to get articles list. Server responded with error ${error.error}`,
+              'close'
             );
             return of(
               ReaderApiActions.getArticlesListFailed({ error: error.error })
