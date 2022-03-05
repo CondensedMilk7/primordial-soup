559998720401d710a515be2c86751b551ee772aa
pridon <fridontetradze@gmail.com>
2022-03-05 21:26:53 +0400

    feat(contact): set up initial form control

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,7 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
 })
-export class ContactComponent {}
+export class ContactComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    surname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', Validators.required),
+  });
+
+  onSubmit() {
+    console.log(this.contactForm.value);
+  }
+}
