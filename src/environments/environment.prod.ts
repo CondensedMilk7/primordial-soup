f49f99329f9e97404f0a78a882639f914f2f0ba1
CondensedMilk7 <82287873+CondensedMilk7@users.noreply.github.com>
2022-03-11 21:20:10 +0400

    fix(environment): missing production variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  snackBarDuration: 5000,
+  githubUrl:
+    'https://raw.githubusercontent.com/CondensedMilk7/mock-articles/master/',
+  libgenUrl: 'https://libgen.fun/search.php?req=',
+  scihubUrl: 'https://sci-hub.se/',
 };
