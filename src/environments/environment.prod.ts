866e38822113771d7f5c064385cd9c2e8349843b
CondensedMilk7 <82287873+CondensedMilk7@users.noreply.github.com>
2022-03-16 19:21:29 +0400

    feat(app): add footer with social media links (#17)
    * feat(app): add footer
    
    * fix(contact): footer not at the bottom
    
    * chore(app): remove extra styles
    
    * feat(footer): add facebook link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -12,4 +12,13 @@ export const environment = {
   contactUrl: 'https://formspree.io/f/xlezlgjw',
   libgenUrl: 'https://libgen.fun/search.php?req=',
   scihubUrl: 'https://sci-hub.se/',
+  socialLinks: {
+    facebook: 'https://www.facebook.com/Primordial-Soup-107806831868869',
+    instagram: '',
+    youtube: 'https://www.youtube.com/channel/UCXu6mQQurrnJXPcoX_oOmZA',
+    githubRepo: 'https://github.com/CondensedMilk7/primordial-soup',
+  },
+  donationLinks: {
+    paypal: 'https://paypal.me/primordialpridon',
+  },
 };
